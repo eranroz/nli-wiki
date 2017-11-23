b00709035a01163f159450d53e863efe664eb6f2
tools.nli-wiki <tools.nli-wiki@tools-bastion-03.tools.eqiad.wmflabs>
2017-11-23 18:08:12 +0000

    removing sample code, changing to format reable to tools lab

--- web/nli_app/frontend.py
+++ web/nli_app/frontend.py
@@ -10,8 +10,8 @@ from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
 from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
 from markupsafe import escape
 
-from .forms import SignupForm
-from .nav import nav
+#from .forms import SignupForm
+from nav import nav
 
 frontend = Blueprint('frontend', __name__)
 
@@ -21,8 +21,8 @@ frontend = Blueprint('frontend', __name__)
 nav.register_element('frontend_top', Navbar(
     View('Flask-Bootstrap', '.index'),
     View('Home', '.index'),
-    View('Forms Example', '.example_form'),
-    View('Debug-Info', 'debug.debug_root'),
+    #View('Forms Example', '.example_form'),
+    #View('Debug-Info', 'debug.debug_root'),
     Subgroup(
         'Docs',
         Link('Flask-Bootstrap', 'http://pythonhosted.org/Flask-Bootstrap'),
@@ -45,21 +45,3 @@ def index():
     return render_template('index.html')
 
 
-# Shows a long signup form, demonstrating form rendering.
-@frontend.route('/example-form/', methods=('GET', 'POST'))
-def example_form():
-    form = SignupForm()
-
-    if form.validate_on_submit():
-        # We don't have anything fancy in our application, so we are just
-        # flashing a message when a user completes the form successfully.
-        #
-        # Note that the default flashed messages rendering allows HTML, so
-        # we need to escape things if we input user values:
-        flash('Hello, {}. You have successfully signed up'
-              .format(escape(form.name.data)))
-
-        # In a real application, you may wish to avoid this tedious redirect.
-        return redirect(url_for('.index'))
-
-    return render_template('signup.html', form=form)
